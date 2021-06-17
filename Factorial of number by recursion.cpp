1d2bf7f1a58472a2d598f075c084388ed888007d
techieritesh <73749372+techieritesh@users.noreply.github.com>
2021-06-17 21:38:23 +0530

    Factorial of number by recursion

---
+++ Factorial of number by recursion.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int fact(int n)
+{
+	if(n==1)
+	{
+		return 1;
+	}
+	return n*fact(n-1);
+}
+int main()
+{
+	int n;
+	cout<<"Enter the number";
+	cin>>n;
+	
+
+cout<<"The factorial of number:"<<fact(n);
+return 0;
+}
