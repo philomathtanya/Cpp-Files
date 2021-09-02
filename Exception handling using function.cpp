328a9631d9dbff4925ffd21c0a14b2f7494fa7b6
techieritesh <73749372+techieritesh@users.noreply.github.com>
2021-09-02 22:37:10 +0530

    Exception handling using function

---
+++ Exception handling using function.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void fun()
+{
+	throw 10;
+}
+int main()
+{
+	cout<<"First line";
+	try{
+			fun();
+		cout<<"\nhello";
+	
+	}
+	catch(int b){
+		cout<<"\nException number is:"<<b;
+	}
+}
